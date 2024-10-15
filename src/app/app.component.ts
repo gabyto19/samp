926dbca459cf0c3438a947995a17bb8b13df977f
gabyto19 <gabuniagoga19@gmail.com>
2024-10-16 00:18:24 +0400

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,32 @@
-import { Component,HostListener } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']  // Corrected styleUrls
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'samp';
-  
   isScrolled = false;
-
+  footerClass: string = '';  // Initialize footerClass as a string
+  
   @HostListener('window:scroll', [])
   onWindowScroll() {
-    this.isScrolled = window.scrollY > 50; // Change 50 to the number of pixels after which the color changes
+    this.isScrolled = window.scrollY > 50; // Trigger based on scroll position
   }
 
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (event.urlAfterRedirects.includes('/start-game')) {
+          this.footerClass = 'start-game-footer';  // Assign class based on route
+        } else {
+          this.footerClass = 'welcome-page';  // Reset class for other routes
+        }
+      }
+    });
+  }
 }
